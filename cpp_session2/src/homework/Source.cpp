181a5fd96e7792194ca0679f76958eada9274040
rfarmer-nexon <rfarmer@nexon.com>
2019-08-01 18:47:19 -0700

    initial setup for day 2 homework
    I also updated the gitignore for vscode

---
+++ cpp_session2/src/homework/Source.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+
+*/
+
+void question4() {
+
+}
+
+void question5() {
+
+}
+
+void question10() {
+
+}
+
+int main() {
+	question4();
+	question5();
+	question10();
+}

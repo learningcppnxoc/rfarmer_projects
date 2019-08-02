7dfe4a35ac2703bf91bae4edc2ea12c504a1b95c
rfarmer-nexon <rfarmer@nexon.com>
2019-08-01 18:54:09 -0700

    Added a comment to clear up any confusion with answering questions

--- cpp_session2/src/homework/Source.cpp
+++ cpp_session2/src/homework/Source.cpp
@@ -3,7 +3,14 @@
 using namespace std;
 
 /*
-
+	Please feel free to add more functions to your
+	code if you would like to test out more of the
+	questions.  Make sure to keep the new functions
+	above the main() function and then to reference
+	them back in the main function.  It is also
+	recommended to comment out functions you are
+	not using to more accurately test out your
+	code.
 */
 
 void question4() {

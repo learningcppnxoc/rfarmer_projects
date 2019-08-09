e12db09ede7347b8093230e224fa0e50790cdbd0
rfarmer-nexon <rfarmer@nexon.com>
2019-08-08 18:32:10 -0700

    cpp_session3

---
+++ cpp_session3/src/homework/Source.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+C++ Session 3 Homework
+1. Compile, execute, and test the programs in this chapter.
+
+2. Are the following definitions valid? Why or why not?
+
+	const std::string hello = "Hello";
+	const std::string message = hello = ", world" + "!";
+
+3. Are the following definitions valid? Why or why not?
+
+	const std::string exclam = "!";
+	const std::string message = "Hello" + ", world" + exclam;
+*/
+
+void question6() {
+
+}
+
+
+int main() {
+	question6();
+}

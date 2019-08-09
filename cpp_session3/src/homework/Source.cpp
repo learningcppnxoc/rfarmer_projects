6d107b6a65a12c5424437cdd217de2ef3c897cd7
rfarmer-nexon <rfarmer@nexon.com>
2019-08-08 18:58:30 -0700

    Updated Source.cpp and added Debug to gitignore

--- cpp_session3/src/homework/Source.cpp
+++ cpp_session3/src/homework/Source.cpp
@@ -52,12 +52,41 @@ int main() {
 	{ const std::string s = "a string";
 	  std::cout << s << std::endl;
 	{ const std::string s = "another string";
+	  std::cout << s << std::endl; }
+	return 0;
+}
+
+5. What about this one? What if we change }} to };} in the third line
+from the end?
+#include <iostream>
+
+int main()
+{
+	{ const std::string s = "a string";
+	  std::cout << s << std::endl;
+	{ const std::string s = "another string";
 	  std::cout << s << std::endl; }}
 	return 0;
 }
 
-5. What about this one? What if we change }} to };} in the third line from the end?
+6. What does the following program do if, when it asks you for input,
+you type two names (for example, Samuel Beckett)? Predict the behavior
+before running the program, then try it.
 
+#include <iostream>
+
+int main()
+{
+	std::cout << "What is your name? ";
+	std::string name;
+	std::cin >> name;
+	std::cout << "Hello. " << name
+	          << std::endl << "And what is yours? ";
+	std::cin >> name;
+	std::cout << "Hello, " << name
+	          << "; nice to meet you too!" << std::endl;
+	return 0;
+}
 
 */
 
@@ -68,4 +97,7 @@ void question6() {
 
 int main() {
 	question6();
+
+	cout << "Press enter to continue..." << endl;
+	cin.get();
 }
